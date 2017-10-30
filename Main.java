3332fa0288c8571814dcbbffc8359a628e3bbc7f
PingWu <wp571988@163.com>
2017-10-30 21:45:25 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * @author WuPing
+ * @version 2017年10月30日 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		 System.out.println("Hello World!");
+	}
+  
+}
